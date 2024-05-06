d5f489a469ce7cc518a244ab852dd2e17a7a6861
prashanthimudepaka <166690972+prashanthimudepaka@users.noreply.github.com>
2024-05-06 10:42:40 +0530

    Add files via upload
    functional and class components simple export and export default 
    
    first create react app
    npx create-react-app appname
    npm install
    these both commands in terminal will install the necessary dependencies
    then simply create a components folder in src and add greet.js, welcome.js files in it
    modify app.js as above file thats it 
    npm start --> to execute

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from "./components/Greet";
+import { Component } from 'react';
+import Welcome from './components/Welcome';
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Greet/>
+        <Welcome/>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ Greet.js
@@ -0,0 +1,5 @@
+import React from "react"
+function Greet(){
+    return <h1>hello prashanthi</h1>
+}
+export default Greet;
